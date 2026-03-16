b624ba819de916e47a0bce14e3d6f0a0ad712a99
Xuchen Pan <32844285+pan-x-c@users.noreply.github.com>
2026-03-16 13:58:52 +0800

    feat(CLI): Add `copaw update` command to update CoPaw automatically (#1278)

--- src/copaw/cli/main.py
+++ src/copaw/cli/main.py
@@ -101,6 +101,16 @@ from .desktop_cmd import desktop_cmd  # noqa: E402
 
 _record(".desktop_cmd", time.perf_counter() - _t)
 
+_t = time.perf_counter()
+from .update_cmd import update_cmd  # noqa: E402
+
+_record(".update_cmd", time.perf_counter() - _t)
+
+_t = time.perf_counter()
+from .shutdown_cmd import shutdown_cmd  # noqa: E402
+
+_record(".shutdown_cmd", time.perf_counter() - _t)
+
 _total = time.perf_counter() - _t0_main
 _init_timings.append(("(total imports)", _total))
 logger.debug("%.3fs (total imports)", _total)
@@ -152,3 +162,5 @@ cli.add_command(models_group)
 cli.add_command(skills_group)
 cli.add_command(uninstall_cmd)
 cli.add_command(desktop_cmd)
+cli.add_command(update_cmd)
+cli.add_command(shutdown_cmd)
