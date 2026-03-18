7522a52b16e4c834cfe7ea246698a31f37879b2d
hongxicheng <1003394729@qq.com>
2026-03-18 14:45:53 +0800

    fix(wecom): Modify the initialization method of WeCom client (#1706)

--- src/copaw/app/channels/wecom/channel.py
+++ src/copaw/app/channels/wecom/channel.py
@@ -27,7 +27,7 @@ from agentscope_runtime.engine.schemas.agent_schemas import (
     ImageContent,
     TextContent,
 )
-from aibot import WSClient, WSClientOptions, generate_req_id
+from wecom_aibot_sdk import WSClient, generate_req_id
 
 from ....constant import DEFAULT_MEDIA_DIR
 from ..base import (
@@ -776,12 +776,11 @@ class WecomChannel(BaseChannel):
             )
 
         self._loop = asyncio.get_running_loop()
-        options = WSClientOptions(
+        self._client = WSClient(
             bot_id=self.bot_id,
             secret=self.secret,
             max_reconnect_attempts=self._max_reconnect_attempts,
         )
-        self._client = WSClient(options)
 
         # Register event handlers
         self._client.on("message", self._on_message_sync)
